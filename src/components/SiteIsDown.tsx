4fd46c2611804a9dd9cf4280267c7c7d433788b4
Amrit-Raj12 <amrit.raj1224@gmail.com>
2025-03-31 16:19:53 +0530

    Dev - fixed build issue

--- src/components/SiteIsDown.tsx
+++ src/components/SiteIsDown.tsx
@@ -4,7 +4,7 @@ const SiteIsDown: React.FC = () => {
   return (
     <div className="flex items-center justify-center min-h-screen bg-cover bg-center" style={{ backgroundImage: "url('/assets/down.webp')" }}>
       <div className="bg-[#322514] bg-opacity-80 p-8 rounded-2xl shadow-lg text-center max-w-lg">
-        <h1 className="text-4xl font-bold text-primary mb-4">We're Down for Maintenance</h1>
+      <h1 className="text-4xl font-bold text-primary mb-4">We&apos;re Down for Maintenance</h1>
         <p className="text-white mb-6">
           Our site is currently undergoing maintenance. We apologize for the inconvenience and appreciate your patience.
         </p>
